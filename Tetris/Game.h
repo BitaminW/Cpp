194ee94c924ee922d37cd171a640948889354c1a
BitaminW <114838001+BitaminW@users.noreply.github.com>
2022-11-23 02:07:49 +0900

    Create Game.h

---
+++ Tetris/Game.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+#include <time.h>
+
+#define DISPLAY_X      1
+#define DISPLAY_Y      1
+
+enum GameStaus{
+	GAME_OVER, GAME_START, GAME_PAUSE, GAME_RESTART
+};
+
+// 화면 관리
+class DisplayManager {
+
+public:
+	DisplayManager() {}
+	void gotoxy(unsigned int x, unsigned int y);
+	void drawTitle();
+};
+
+// 게임의 흐름
+class GameManager{
+
+private:
+	DisplayManager* display_manager;
+	
+public:
+	GameManager() {
+		display_manager = new DisplayManager();
+		display_manager->drawTitle();
+	}
+	
+
+	/*void gameRun() {
+		_display_manager
+	}*/
+
+	// 디스플레이 그리기 
+
+
+
+};
+
+
+// 도형 클래스.
+class Block {
+
+};
+
+
+
+// 게임 점수 , 게임 오버 관리
+class ScoreManger {
+
+};
+
